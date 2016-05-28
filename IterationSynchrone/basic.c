ad366ce24857cc3e36b29c0cbf2dfe72b21e7747
amilliet <ablondel0002@emi.u-bordeaux1.fr>
2016-05-28 22:26:19 +0200

    test

--- IterationSynchrone/basic.c
+++ IterationSynchrone/basic.c
@@ -4,7 +4,7 @@
 int traiter(int y_d, int x_d, int y_f, int x_f, unsigned ocean[DIM][DIM], c couleurs[DIM][DIM]){
     int changement = 0;
 
-#pragma omp parallel for collapse(2) reduction(+:ocean[y][x],ocean[y][x-1],ocean[y][x+1],ocean[y-1][x],ocean[y + 1][x])
+#pragma omp parallel for collapse(2) //reduction(+:ocean[y][x],ocean[y][x-1],ocean[y][x+1],ocean[y-1][x],ocean[y + 1][x])
     for (int y = y_d; y < y_f; y++)
     {
         for (int x = x_d; x < x_f ; x++){
@@ -27,8 +27,8 @@ int traiter(int y_d, int x_d, int y_f, int x_f, unsigned ocean[DIM][DIM], c coul
             coloring(y+1,x,move, ocean, couleurs);
             coloring(y,x,move,ocean, couleurs);
 #endif
-            int tid = omp_get_thread_num();
-            printf("Hello World from thread = %d\n", tid);
+            //int tid = omp_get_thread_num();
+            //printf("Hello World from thread = %d\n", tid);
         }
         
     }
