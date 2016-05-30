b5a4a3d6b4ff993251a00dfc308b595454cbc2e8
Angélique Blondel <ablondel0002@enseirb.fr>
2016-05-30 11:25:42 +0200

    test

--- IterationSynchrone/other_sequentielle.c
+++ IterationSynchrone/other_sequentielle.c
@@ -111,6 +111,7 @@ int traiter_sequentielle (int y_d, int x_d, int y_f, int x_f, unsigned ocean[DIM
              coloring(y+1,x,move,ocean,couleurs);
 #endif
         }
+#pragma barrier
         
     }
     }
