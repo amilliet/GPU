d03739faa82f3efb8282530ef1db39e6a009f62e
Angélique Blondel <ablondel0002@enseirb.fr>
2016-06-04 14:45:53 +0200

    err

--- PARTIE3_ANGELIQUE/propagation.c
+++ PARTIE3_ANGELIQUE/propagation.c
@@ -138,7 +138,7 @@ int traiterProp(int y_d, int x_d, int y_f, int x_f, unsigned ocean[DIM][DIM], c 
                 }
                 cursor++;
             }
-        }else if ((my_num+1)*nb_lines< DIM ) {
+        }else if ((my_num+1)*nb_lines >= DIM ) {
             for(int y = my_num*nb_lines-DEPTH; y < FIN; y++){
                 for (int x = DEBUT; x < FIN; x++){
                     tmp[cursor][x] = ocean[y][x];
