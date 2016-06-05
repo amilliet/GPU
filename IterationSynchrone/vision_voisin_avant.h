0728048fd600e6d1c8b30585e840d771ee0282c6
Angélique Blondel <ablondel0002@enseirb.fr>
2016-06-05 17:39:21 +0200

    test

--- IterationSynchrone/vision_voisin_avant.h
+++ IterationSynchrone/vision_voisin_avant.h
@@ -10,6 +10,6 @@
 
 
 int traiter_vision_voisin_avant_sequentielle (int y_d, int x_d, int y_f, int x_f, unsigned ocean[DIM][DIM], c couleurs[DIM][DIM]);
-int traiter_vision_voisin_avant_parallele (int y_d, int x_d, int y_f, int x_f, unsigned ocean[DIM][DIM],c couleurs[DIM][DIM], int nb_threads)
+int traiter_vision_voisin_avant_parallele (int y_d, int x_d, int y_f, int x_f, unsigned ocean[DIM][DIM],c couleurs[DIM][DIM], int nb_threads);
 
 #endif /* other_sequentielle_h */
